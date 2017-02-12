b3e64b252274583037a3b07830c456cf010d22ef
Spencer Hubbard <shubbard@shubbard-mac.local>
2017-02-11 16:32:05 -0800

    Added idex and style sheet

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Michael Woody's Website</title>
+    <!--
+    <link href="images/icon.gif" type="image/gif" rel="shortcut icon" />
+    -->
+    <link href="style.css" type="text/css" rel="stylesheet" />
+  </head>
+  <body>
+    <p>Just a place holder!</p>
+  </body>
+</html>
