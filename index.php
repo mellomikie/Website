410150ff0ac1bc75818573c1299683f8d427048a
Spencer Hubbard <shubbard@shubbard-mac.local>
2017-02-12 20:01:26 -0800

    Added about Michael section to index

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
 include("common.php");
 
 head();
-placeholder_message();
-foot();
 ?>
+
+<div id="about">
+  <h1>About Michael</h1>
+  <p>
+    Michael is a creative thinker and designer. With a background in Psychology,
+    he approaches design with a balance of research, behavioral perspective,
+    creative problem solving and close attention to detail to develop deliberate
+    and effective solutions. He is thoughtful and passionate about crafting user
+    experiences, brand identities and engaging visual designs. His love for
+    travel and years living in Japan influence his approach to design.
+  </p>
+</div>
+
+<?php foot(); ?>
