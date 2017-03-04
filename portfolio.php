6bf18abe81f08a3f06bff2cfb5d05f7cfc9b73d1
Spencer Hubbard <hubbards@users.noreply.github.com>
2017-03-04 14:31:02 -0800

    Added Texas art and film project page

--- portfolio.php
+++ portfolio.php
@@ -7,8 +7,9 @@ include("Project.php");
 # TODO add projects to portfolio
 $posters = new Project("Posters", "posters.php", "images/posters/postersfeaturedimageslow1.gif", "Posters");
 $antivaxx = new Project("Who are the Anti-Vaxxers?", "antivaxx.php", "images/antivaxx/AVfeaturedimage1.gif", "Anti-vaxxers");
+$taf = new Project("Texas Art &amp; Film", "taf.php", "images/taf/taf_ftd_image_1000x1000.jpg", "Texas Art &amp; Film");
 
-$portfolio = array($posters, $antivaxx);
+$portfolio = array($posters, $antivaxx, $taf);
 ?>
 
 <div id="portfolio">
---
+++ taf.php
@@ -0,0 +1,74 @@
+<?php
+# This is the PHP file for the Texas art and film project page of Michael's
+# website.
+
+include("common.php");
+
+$styles = array("styles/taf.css");
+
+head($styles);
+?>
+
+<img src="images/taf/taf_intro_image_desktop-1024x406.jpg" alt="Texas art &amp; film" />
+
+<h1>Texas Art &amp; Film</h1>
+
+<p>
+  Texas Art &amp; Film is an online film editorial publication for film reviews
+  and film festival coverage. It needed a UX redesign and branding overhaul. The
+  research required organizing large amounts of content and strategizing to make
+  it accessible to a wide demographic using new features and adding a social
+  media strategy.
+</p>
+
+<img src="images/taf/taf_mobilephonedesktop1.jpeg" alt="Mobile phone app" />
+
+<p>
+  Texas Art &amp; Film is primarily a resource for finding online film reviews.
+  My research showed that the majority of users who are looking for reviews on
+  film access the site on a mobile device, so I designed a layout that gave
+  users easy access to the latest film reviews, provided them with easy mobile
+  sharing options and offered the ability to comment and join in the
+  conversation about their favorite films.
+</p>
+
+<img src="images/taf/taf_oscars_ballots_2016.jpg" alt="Oscars ballots 2016" />
+<img src="images/taf/taf_oscars_ballots_2017.jpg" alt="Oscars ballots 2017" />
+
+<h2>Event Campaigns</h2>
+
+<p>
+  I create custom content for site events, including yearly Oscars campaigns
+  featuring printable ballots, custom page designs, HTML email campaigns and
+  traffic driving graphics to promote the events.
+</p>
+
+<div id="festivals">
+  <img src="images/taf/festivals.png" alt="Festivals" />
+  <div>
+    <h2>Content Management</h2>
+    <p>
+      I organized years of festival coverage into a film festival section that
+      offers various types of content including video interviews, film reviews,
+      and articles about the festival.
+    </p>
+  </div>
+</div>
+
+<div id="website">
+  <img src="images/taf/website.jpeg" alt="Website" />
+
+  <div id="visit">
+    <a href="http://www.texasartfilm.net">Visit Website</a>
+  </div>
+
+  <p>
+    After analyzing the content and user research, I designed the final layout of
+    the site to offer custom banners that show the user what is new on the site,
+    give quick access to the latest film reviews and provide film festival
+    coverage as well as awards coverage. After the redesign, user traffic
+    increased by 150%.
+  </p>
+</div>
+
+<?php foot(); ?>
